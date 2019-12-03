5a813d986a07c673242e1e6dc86737da26805201
JohnnyBaptista <joaosbaptista@hotmail.com>
2019-12-02 23:38:10 -0300

    Insercao de tipo

--- FrontEnd/codeblobs/src/app.routes.js
+++ FrontEnd/codeblobs/src/app.routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Groups from './views/Groups/Groups';
 import GroupsTable from './views/Groups/GroupsTable';
 import GroupForm from "./views/Groups/GroupForm";
+import TypesForms from "./views/Types/TypesForms";
 
 const AppRoute = props => {
   return (
@@ -12,6 +13,7 @@ const AppRoute = props => {
         <Route exact path="/" component={Groups} />
         <Route exact path="/group/:group_id" component={GroupsTable} />
         <Route exact path="/insert/group" component={GroupForm} />
+        <Route exact path="/insert/type" component={TypesForms} />
       </Switch>
     </Router>
   );
--- FrontEnd/codeblobs/src/views/Types/Types.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-
-export default class Types extends Component {
-  render() {
-    return <div/>;
-  }
-}
